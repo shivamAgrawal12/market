dcb88b2e38ecbc8bf2ddfa9fc7a0f224db71d0ef
Shivam Agrawal <51692867+shivamAgrawal12@users.noreply.github.com>
2026-01-05 12:35:45 +0530

    news

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Login from "./component/Login";
+import React from "react";
 import First from "./component/First";
 
 const App = () => {
-  const [isReady, setIsReady] = useState(false);  
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    // wait for Login to run validity check
-    const status = localStorage.getItem("auth_ready");
-    if (status === "true") {
-      setLoggedIn(true);
-    }
-    setIsReady(true);
-  }, []);
-
-  if (!isReady) {
-    return (
-      <div style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#0b1120",
-        color: "white"
-      }}>
-        Loading...
-      </div>
-    );
-  }
-
-  return loggedIn ? (
-    <First/>
-  ) : (
-    <Login onSuccess={() => setLoggedIn(true)} />
+  return (
+    <First />
   );
 };
 
